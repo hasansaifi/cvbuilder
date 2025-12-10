356653486e9089fa05bcc5a5840be17082764a0d
hasansaifi <ibnehasan789@gmail.com>
2025-12-10 23:32:50 +0300

    EOD

---
+++ src/components/LeftSidebar.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from '../assets/react.svg'
+import viteLogo from '/vite.svg'
+import '../App.css'
+
+function LeftSidebar() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="section leftSidebar">
+
+    </div>
+  )
+}
+
+export default LeftSidebar
